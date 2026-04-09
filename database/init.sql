1fc0ee2383207167f595f6781806e6732c3acf07
nicolas097 <nicolasolivares097@gmail.com>
2026-04-09 02:44:58 -0400

    fix: permisos de admin en el ddl

--- database/init.sql
+++ database/init.sql
@@ -1,5 +1,15 @@
-CREATE TABLE empleado (
+CREATE TABLE IF NOT EXISTS empleado (
     id_empleado serial primary key,
     nombres varchar not null,
     apellidos varchar not null
 );
+
+
+ALTER TABLE empleado OWNER TO postgres;
+
+-- 3. Permisos de acceso para tu usuario personal de IAM
+-- Esto asegura que cuando entres como nico.gcp.dev@gmail.com tengas control total.
+GRANT ALL PRIVILEGES ON TABLE empleado TO "nico.gcp.dev@gmail.com";
+
+-- 4. Permisos para el grupo general de usuarios IAM (Opcional pero recomendado)
+GRANT ALL PRIVILEGES ON TABLE empleado TO cloudsqliamuser;
